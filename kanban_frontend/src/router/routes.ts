7b0e381dc6807d6e2fc15bb07bc0389c1888926d
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-02-02 02:41:01 -0300

    feat: create page and update route

--- kanban_frontend/src/router/routes.ts
+++ kanban_frontend/src/router/routes.ts
@@ -5,7 +5,8 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/LoginPage.vue') }, //TODO autoredirect to /whatever if logged in
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
     ],
   },
 
