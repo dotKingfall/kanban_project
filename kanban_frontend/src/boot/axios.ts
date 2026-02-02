e918e9bb4c202754924cb3f855e68ceb46c7398d
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-02-01 23:27:54 -0300

    feat: update CORS file

--- kanban_frontend/src/boot/axios.ts
+++ kanban_frontend/src/boot/axios.ts
@@ -14,7 +14,13 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://localhost:8000/api' });
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  withCredentials: true,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+});
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
