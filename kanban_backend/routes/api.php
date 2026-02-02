c36dffc9bbca3bad2c0241677221fdc71a588313
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-02-02 01:06:46 -0300

    fix: install sanctum

--- kanban_backend/routes/api.php
+++ kanban_backend/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
     Route::get('/user', function (Request $request) {
