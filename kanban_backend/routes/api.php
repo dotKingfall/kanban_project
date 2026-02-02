39f5039abf59da193e38098d6c5970d49d2f8ba9
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-02-01 23:48:14 -0300

    feat: add controller and api routes

---
+++ kanban_backend/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // TODO ADD CORE ROUTES LATER
+});
